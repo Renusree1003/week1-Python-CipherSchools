becc22746f2977456e6bc785db176561aba69810
Renusree1003 <118983655+Renusree1003@users.noreply.github.com>
2022-11-30 15:05:32 +0530

    Create Class 5-Operatos and Expressions-CipherSchools.py

---
+++ Class 5-Operatos and Expressions-CipherSchools.py
@@ -0,0 +1,76 @@
+5 + 5
+10
+
+10-5
+5
+isinstance(10.0,int)
+False
+10 / 2
+5.0
+
+10 // 3
+3
+
+2*3
+6
+
+2!*3
+8
+
+"Renu" + "Sree"
+"Renu Sree"
+
+"abc" * 6
+"abcabcabcabcabcabc"
+
+1 == 2
+False
+
+1 != 2
+True
+
+1 < 2
+True
+
+2 > 3
+False
+
+"ab" < "z"
+True
+
+"" or 2.5
+2.5
+
+bool("")
+False
+
+a or b = a (if a is truthy)
+a or b = b (if a is falsy)
+a and b = b(if a is truthy)
+a and b =a (if a is falsy)
+
+true or b = true
+false or b = b
+true and b = b
+false and b = false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
